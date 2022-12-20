d49d17e184c1bbf8362ec00a8b7e34e97b8f7daa
Exxclud <tronin371@gmail.com>
2022-12-20 21:02:27 +0300

    Add user model

--- api_yamdb/user/urls.py
+++ api_yamdb/user/urls.py
@@ -1,16 +1,8 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
-from .views import get_jwt, send_code, UsersViewSet, UserAdminViewSet
+from .views import RegistrationAPIView
 
-app_name = "user"
-
-router = DefaultRouter()
-router.register("users", UserAdminViewSet, basename="users")
-
+app_name = 'user'
 urlpatterns = [
-    path("auth/sigup/", send_code),
-    path("auth/token/", get_jwt),
-    path("users/me/", UsersViewSet.as_view()),
-    path("", include(router.urls)),
+    path('auth/', RegistrationAPIView.as_view()),
 ]
--- api_yamdb/user/views.py
+++ api_yamdb/user/views.py
@@ -1,107 +1,22 @@
-import random
-
-from django.conf import settings
-from django.contrib.auth.hashers import check_password, make_password
-from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404
-from rest_framework import status, permissions, viewsets, filters
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import AccessToken
 
-from .models import User
-from .permissions import IsAdmin
-from .serializers import SendCodeSerializer, UserSerializer
+from .renderers import UserJSONRenderer
+from .serializers import RegistrationSerializer
 
 
-@api_view(["POST"])
-@permission_classes([AllowAny])
-def get_jwt(request):
-    username = request.data.get("username")
-    confirmation_code = request.data.get("confirmation_code")
-    if not username or not confirmation_code:
-        return Response(
-            "Одно или несколько обязательных полей пропущены",
-            status=status.HTTP_400_BAD_REQUEST,
-        )
+class RegistrationAPIView(APIView):
+    renderer_classes = (UserJSONRenderer,)
+    permission_classes = (AllowAny,)
+    serializer_class = RegistrationSerializer
 
-    if not User.objects.filter(username=username).exists():
-        return Response("Имя пользователя неверное", status=status.HTTP_404_NOT_FOUND)
+    def post(self, request):
+        user_name = request.data.get('user', {})
 
-    user = User.objects.get(username=username)
-    if check_password(confirmation_code, user.confirmation_code):
-        token = AccessToken.for_user(user)
-        return Response({"access": str(token)})
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-    return Response("Код подтверждения неверен", status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["POST"])
-@permission_classes([AllowAny])
-def send_code(request):
-    username = request.data.get("username", False)
-    email = request.data.get("email", False)
-    if not username or not email:
-        return Response(
-            "Одно или несколько обязательных полей пропущены",
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-    serializer = SendCodeSerializer(data=request.data)
-    if serializer.is_valid():
-        confirmation_code = "".join(map(str, random.sample(range(10), 6)))
-        user = User.objects.filter(username=username).exists()
-        if not user:
-            User.objects.create_user(email=email, username=username)
-        User.objects.filter(email=email).update(
-            confirmation_code=make_password(
-                confirmation_code, salt=None, hasher="default"
-            )
-        )
-        mail_subject = "Код подтверждения для доступа к API! "
-        message = f"""
-                Здравствуйте!
-                Код подтверждения для доступа к API: {confirmation_code}
-                С уважением
-                Yamdb
-                """
-        send_mail(
-            mail_subject,
-            message,
-            settings.EMAIL_HOST_USER,
-            [email],
-            fail_silently=False,
-        )
-        text_message = f"Код отправлен на адрес {email}." " Проверьте раздел SPAM"
-        return Response(text_message, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class UsersViewSet(APIView):
-    @permission_classes([permissions.IsAuthenticated])
-    def get(self, request):
-        user = get_object_or_404(User, id=request.user.id)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-    @permission_classes([permissions.IsAuthenticated])
-    def patch(self, request):
-        user = get_object_or_404(User, id=request.user.id)
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class UserAdminViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    lookup_field = "username"
-    filter_backends = [filters.SearchFilter]
-    permission_classes = [IsAdmin]
-    search_fields = [
-        "user__username",
-    ]
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
