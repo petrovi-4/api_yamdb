60cc8aaf820bdfe0a2e66f00ce7d7db7ab14c6d9
Exxclud <tronin371@gmail.com>
2022-12-22 21:56:15 +0300

    Поправлена модель и добавлено валидация имени для регистрации

--- api_yamdb/user/permissions.py
+++ api_yamdb/user/permissions.py
@@ -11,6 +11,11 @@ class IsAdminOrReadOnly(BasePermission):
         )
 
 
+class IsAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_admin or request.user.is_staff
+
+
 class IsAuthorOrModeratorOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
