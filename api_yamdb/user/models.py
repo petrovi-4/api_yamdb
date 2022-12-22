5de0fccce5810db048e692de140ea3d58f4385d8
Exxclud <tronin371@gmail.com>
2022-12-22 15:01:17 +0300

    Добавлена User модель. Написано API под нее. Добавлена регистрация

--- api_yamdb/user/models.py
+++ api_yamdb/user/models.py
@@ -1,43 +1,13 @@
-from datetime import datetime, timedelta
-
-import jwt
-from django.conf import settings
+from django.contrib import admin
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import BaseUserManager
 from django.core.validators import RegexValidator
 from django.db import models
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, username, email):
-        if username is None:
-            raise TypeError("Users must have a username.")
-
-        if email is None:
-            raise TypeError("Users must have an email address.")
-
-        user = self.model(username=username, email=self.normalize_email(email))
-        user.save()
-
-        return user
-
-    def create_superuser(self, username, email, password):
-        if password is None:
-            raise TypeError("Superusers must have a password.")
-
-        user = self.create_user(username, email)
-        user.is_superuser = True
-        user.is_admin = True
-        user.save()
-
-        return user
 
 
 class User(AbstractUser):
     ADMIN = "admin"
     MODERATOR = "moderator"
     USER = "user"
-
     ROLES = (
         (ADMIN, "Администратор"),
         (MODERATOR, "Модератор"),
@@ -55,7 +25,9 @@ class User(AbstractUser):
         verbose_name="Ник пользователя",
     )
     email = models.EmailField(
-        unique=True, max_length=254, verbose_name="Адрес электронной почты"
+        unique=True,
+        max_length=254,
+        verbose_name="Адрес электронной почты",
     )
     first_name = models.CharField(
         max_length=100, blank=True, verbose_name="Имя пользователя"
@@ -67,7 +39,7 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=100, verbose_name="Роль", choices=ROLES, default=USER
     )
-    objects = UserManager()
+    confirmation_code = models.CharField(max_length=6, default='000000')
 
     def __str__(self):
         return self.username
@@ -84,24 +56,5 @@ class User(AbstractUser):
     def is_user(self):
         return self.role == self.USER
 
-    @property
-    def token(self) -> str:
-        """
-        Позволяет получить токен пользователя путем вызова user.token, вместо
-        user._generate_jwt_token().
-        """
-        return self._generate_jwt_token()
 
-    def _generate_jwt_token(self) -> str:
-        """
-        Генерирует веб-токен JSON, срок действия токена составляет 1 день от создания
-        """
-        dt = datetime.now() + timedelta(days=1)
-
-        token = jwt.encode(
-            {"id": self.pk, "exp": int(dt.strftime("%s"))},
-            settings.SECRET_KEY,
-            algorithm="HS256",
-        )
-
-        return token.decode("utf-8")
+admin.site.register(User)
