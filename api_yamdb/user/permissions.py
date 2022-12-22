d37527f918d1b498b066d785103d5b8b976d96c8
Exxclud <tronin371@gmail.com>
2022-12-22 15:03:52 +0300

    Добавлены все требуемые пермишены

--- api_yamdb/user/permissions.py
+++ api_yamdb/user/permissions.py
@@ -1,14 +1,4 @@
-from rest_framework import permissions
 from rest_framework.permissions import BasePermission
-
-
-class IsAdminOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.user.is_admin
-            or request.user.is_staff
-            or request.method in permissions.SAFE_METHODS
-        )
 
 
 class IsAdmin(BasePermission):
