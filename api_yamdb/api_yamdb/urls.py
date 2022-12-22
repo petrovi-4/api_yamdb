6b5877cd00b8a769e47eb8540b61ac6858c38d8f
Exxclud <tronin371@gmail.com>
2022-12-22 15:01:17 +0300

    Добавлена User модель. Написано API под нее. Добавлена регистрация

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,22 +1,9 @@
-"""YaMDb URL Configuration"""
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.views.generic import TemplateView
-from rest_framework.routers import DefaultRouter
-
-from api_yamdb.views import CategoryViewSet, GenreViewSet, TitleViewSet
-
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet, basename='category')
-router.register(r'genres', GenreViewSet, basename='genre')
-router.register(r'titles', TitleViewSet, basename='title')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
+    path("admin/", admin.site.urls),
+    path("redoc/", TemplateView.as_view(template_name="redoc.html"), name="redoc"),
+    path("api/v1/", include("user.urls")),
 ]
---
+++ api_yamdb/user/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class SendCodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("email", "username")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("email", "username", "first_name", "last_name", "role", "bio")
+        read_only_fields = ("role",)
