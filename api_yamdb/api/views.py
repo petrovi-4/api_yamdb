fa76741ce9dd628be26c554cc622d2ee96c61870
Сергей Мартынов <40.10.444@gmail.com>
2022-12-24 12:36:38 +0300

    merge

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,50 +1,39 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, mixins, viewsets
-from rest_framework.pagination import (PageNumberPagination)
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from seriaizers import CommentSerializer, ReviewSerializer
 
-from api.models import Category, Genre, Title
-from api.seriaizers import (CategorySerializer,
-                            GenreSerializer, TitleSerializer)
-# from user.permissions import IsAdminOrReadOnly
+from users.permissions import IsAuthorOrModeratorOrAdmin
 
-
-class CategoryViewSet(
-    viewsets.GenericViewSet,
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.DestroyModelMixin
-):
-    """Вьюсет категорий"""
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    # permission_classes = (IsAdminOrReadOnly,)
-    pagination_class = PageNumberPagination
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('name',)
-    lookup_field = 'slug'
+from .models import Review, Comment
 
 
-class GenreViewSet(
-    viewsets.GenericViewSet,
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.DestroyModelMixin
-):
-    """Вьюсет жанров"""
-    queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
-    # permission_classes = (IsAdminOrReadOnly,)
-    pagination_class = PageNumberPagination
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('name',)
-    lookup_field = 'slug'
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrModeratorOrAdmin,)
+    pagination_class = LimitOffsetPagination
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        serializer.save(author=self.request.user, review=review)
 
 
-class TitleViewSet(viewsets.ModelViewSet):
-    """Вьюсет постов"""
-    queryset = Title.objects.all()
-    serializer_class = TitleSerializer
-    # permission_classes = (IsAdminOrReadOnly,)
-    pagination_class = PageNumberPagination
-    filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ('name', 'year', 'genre__slug', 'category__slug')
+class ReviewViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrModeratorOrAdmin,)
+    pagination_class = LimitOffsetPagination
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        review_queryset = Review.objects.filter(title=title_id)
+        return review_queryset
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        serializer.save(author=self.request.user, title_id=title_id)
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,24 +1,14 @@
-"""YaMDb URL Configuration"""
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import path
 from django.views.generic import TemplateView
-from rest_framework.routers import DefaultRouter
-
-from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
-
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet, basename='category')
-router.register(r'genres', GenreViewSet, basename='genre')
-router.register(r'titles', TitleViewSet, basename='title')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
+    path('api/', include('api.urls'), name='api'),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
+        name='redoc',
     ),
-    path('api/', include('user.urls', namespace='user')),
+    path('api/', include('users.urls')),
 ]
