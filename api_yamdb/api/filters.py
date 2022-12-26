8162e1ab8023a186fcad887f469dffef3b66a9b2
Alexandr Fedyuninskih <fedyuninskih@yandex.ru>
2022-12-26 23:32:44 +0500

    Сделана фильтрация.

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,13 +1,13 @@
-from django_filters import rest_framework as filters
+import django_filters
 
 from reviews.models import Title
 
 
-class TitleFilter(filters.FilterSet):
-    category = filters.CharFilter(field_name='category__slug')
-    genre = filters.CharFilter(field_name='genre__slug')
-    name = filters.CharFilter(field_name='name', lookup_expr='contains')
-    year = filters.NumberFilter(field_name='year')
+class TitleFilter(django_filters.FilterSet):
+    category = django_filters.CharFilter(field_name='category__slug')
+    genre = django_filters.CharFilter(field_name='genre__slug')
+    name = django_filters.CharFilter(field_name='name', lookup_expr='contains')
+    year = django_filters.NumberFilter(field_name='year')
 
     class Meta:
         model = Title
