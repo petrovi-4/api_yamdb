47bae35fc031875adace51361e27b27fd546fced
Alexandr Fedyuninskih <fedyuninskih@yandex.ru>
2022-12-29 17:48:42 +0500

    Мелкие правки в оформлении.

--- api_yamdb/api/seriaizers.py
+++ api_yamdb/api/seriaizers.py
@@ -8,6 +8,7 @@ from reviews.models import Category, Comment, Genre, Review, Title
 
 User = get_user_model()
 
+
 # В моделиях Category и Genre сменил ключевое поле на 'slug',
 # поскольку оно итак уникальное. Поле 'id' теперь отсутствует.
 
@@ -97,8 +98,8 @@ class ReviewSerializer(serializers.ModelSerializer):
         title_id = self.context['view'].kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
         if (
-            request.method == 'POST'
-            and Review.objects.filter(title=title, author=author).exists()
+                request.method == 'POST'
+                and Review.objects.filter(title=title, author=author).exists()
         ):
             raise serializers.ValidationError(
                 'Вы уже оставили свой отзыв к этому призведению!'
